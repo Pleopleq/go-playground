a95a5a2d9848cc77db2f650054198f4018330abc
Felix Anducho <felix.anducho74@gmail.com>
2023-04-24 21:36:46 -0500

    configure a path for static files

--- web-dev-go/main.go
+++ web-dev-go/main.go
@@ -7,8 +7,12 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(response, "Hello, you've requested: %s\n", request.URL.Path)
+		fmt.Fprintf(response, "Welcome to my website. You've requested: %s\n", request.URL.Path)
 	})
+
+	fs := http.FileServer(http.Dir("static/"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.ListenAndServe(":3000", nil)
 }
