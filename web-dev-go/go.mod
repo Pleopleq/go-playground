58e3691cd5d33645038cae34d29931010a2cdac7
Felix Anducho <felix.anducho74@gmail.com>
2023-04-24 21:47:20 -0500

    Added a new dependency to handle routing

--- web-dev-go/go.mod
+++ web-dev-go/go.mod
@@ -1,3 +1,5 @@
 module tutorial.com/module
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- web-dev-go/main.go
+++ web-dev-go/main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
 		fmt.Fprintf(response, "Welcome to my website. You've requested: %s\n", request.URL.Path)
+	})
+
+	r.HandleFunc("/books/{title}/page/{page}", func(response http.ResponseWriter, request *http.Request) {
+		vars := mux.Vars(request)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(response, "You've requested the book: %s on page %s\n", title, page)
 	})
 
 	fs := http.FileServer(http.Dir("static/"))
