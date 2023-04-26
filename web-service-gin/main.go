ddf9a086262dadf9347f6c3d54fd4d1fd8ebbf70
Felix Anducho <felix.anducho74@gmail.com>
2023-04-25 20:28:06 -0600

    Add post route to handle the addition of a new album

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,19 +13,32 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Selected Ambient Works 85-92", Artist: "Aphex Twin", Price: 29.99},
 	{ID: "3", Title: "Music Has the Right to Children", Artist: "Boards Of Canada", Price: 29.99},
 }
 
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
 func main() {
 	router := gin.Default()
+
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
